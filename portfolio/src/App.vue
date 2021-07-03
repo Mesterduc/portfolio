336e48136add886c57e3d25c97bffdadf67252e1
Hong Duc Cai <hong.duc.cai@gmail.com>
2021-07-03 22:32:27 +0200

    project card and animation

--- portfolio/src/App.vue
+++ portfolio/src/App.vue
@@ -1,9 +1,9 @@
 <template>
-  <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
-  <!-- <router-link to="/hej">hej </router-link> -->
-  <app-nav-bar />
-  <router-view />
-  <!-- {{usestore}} <br>
+	<!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+	<!-- <router-link to="/hej">hej </router-link> -->
+	<app-nav-bar />
+	<router-view />
+	<!-- {{usestore}} <br>
   <input v-model="userInput" type="text" name="" id="">
   <button @click="saveName" type="submit">Submit</button> -->
 </template>
@@ -11,41 +11,39 @@
 <script lang="ts">
 import appNavBar from './layout/navBar/appNavBar.vue'
 import { defineComponent, ref, computed } from 'vue'
-import {useStore} from 'vuex'
+import { useStore } from 'vuex'
 
 export default defineComponent({
-  name: 'App',
-  components: {
-    appNavBar
-  },
-  setup(){
-    // const store = useStore()
-    // const usestore = computed(() => store.state.user.name)
-    // // const usestore2 = computed(() => useStore().state.)
-
-    // const userInput = ref('')
-
-    // function saveName() {
-    //   store.dispatch('saveName', userInput.value)
-    //   userInput.value = ''
-    // }
-    // return { usestore, userInput,saveName}
-
-  }
-
+	name: 'App',
+	components: {
+		appNavBar,
+	},
+	setup() {
+		// const store = useStore()
+		// const usestore = computed(() => store.state.user.name)
+		// // const usestore2 = computed(() => useStore().state.)
+		// const userInput = ref('')
+		// function saveName() {
+		//   store.dispatch('saveName', userInput.value)
+		//   userInput.value = ''
+		// }
+		// return { usestore, userInput,saveName}
+	},
 })
 </script>
 
 <style>
+html {
+	font-size: 10px;
+}
 #app {
-  font-size: 10px;
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  font-size: 10px;
-  width: min((100vw - 64px), 100em);
-    margin: auto;
-  /* text-align: center;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	font-size: 10px;
+	width: min((100vw - 64px), 100em);
+	margin: auto;
+	/* text-align: center;
   color: #2c3e50;
   margin-top: 60px; */
 }
--- portfolio/src/view/Home.vue
+++ portfolio/src/view/Home.vue
@@ -1,103 +1,122 @@
 <template>
-    <h1>This is the Hompepage</h1>
-
-        <section class="filter">
-            <article 
-            class="filter__item"
-            v-for="(category, index) in categories"
-            :class="{ 'filter__item-isActive': index === isActive }"
-				@click="setLinkActive(index)"
-            >
-                {{category}}
-                </article>
-        </section>
-        <section class="projects"></section>
+	<h1>This is the Hompepage</h1>
+	<section class="filter">
+		<article
+			class="filter__item"
+			v-for="(category, index) in categories"
+			:class="{ 'filter__item-isActive': index === isActive }"
+			@click="setLinkActive(index)"
+		>
+			{{ category }}
+		</article>
+	</section>
+	<section class="project__container">
+		<article class="project" v-for="repo in repos">{{ repo.name }}</article>
+	</section>
 </template>
 <script lang="ts">
 import { defineComponent, onMounted, ref } from 'vue'
 import linkIsActive from '../composables/linkIsActive'
 
 export default defineComponent({
-    name: 'Home',
-    setup() {
-        // link active
-        let {isActive, setLinkActive} = linkIsActive()
+	name: 'Home',
+	setup() {
+		// link active
+		let { isActive, setLinkActive } = linkIsActive()
 
-        // array of language categories 
-        const categories = ref(['all', 'vue', 'react', 'javascript', 'swift'])
+		// array of language categories
+		const categories = ref(['all', 'vue', 'react', 'javascript', 'swift'])
 
-        const repos = ref([])
-        const language_url = ref([])
-        const languages = ref([])
-        const content = ref('')
+		const repos = ref([])
+		const language_url = ref([])
+		const languages = ref([])
+		const content = ref('')
 
-        // get Data
-        const getData = async () =>  {
-            const url = 'https://api.github.com/users/Mesterduc/repos'
-            const respose = await fetch(url)
-            const result = await respose.json()
-            // console.log(result)
-            repos.value = result
-            result.forEach(async e => {
-                language_url.value.push(e.languages_url)
-                const url = e.languages_url
-                const respose = await fetch(url)
-                const result = await respose.json()
-                languages.value.push(result)
-            });
-        }
+		// get Data
+		const getData = async () => {
+			const url = 'https://api.github.com/users/Mesterduc/repos'
+			const respose = await fetch(url)
+			const result = await respose.json()
+			// console.log(result)
+			repos.value = result
+			result.forEach(async (e) => {
+				language_url.value.push(e.languages_url)
+				const url = e.languages_url
+				const respose = await fetch(url)
+				const result = await respose.json()
+				languages.value.push(result)
+			})
+		}
 
-         onMounted(getData)
+		onMounted(getData)
 
-        // get readme file
-        const fetchReadme = async () => {
-            const url = 'https://api.github.com/repos/Mesterduc/walkSite/contents/walksite/README.md'
-            const respose = await fetch(url )
-            const result = await respose.json()
-            // console.log(result.content)
-            
-            content.value = atob(result.content)
+		// get readme file
+		const fetchReadme = async () => {
+			const url = 'https://api.github.com/repos/Mesterduc/walkSite/contents/walksite/README.md'
+			const respose = await fetch(url)
+			const result = await respose.json()
+			// console.log(result.content)
 
-        }
-        fetchReadme()
+			content.value = atob(result.content)
+		}
+		fetchReadme()
 
-        // filter background
-        function filterIsActive(){
+		// filter background
+		function filterIsActive() {}
 
-        }
-
-        return {repos, languages, content, language_url, categories, isActive, setLinkActive }
-        
-    },
+		return { repos, languages, content, language_url, categories, isActive, setLinkActive }
+	},
 })
 </script>
 
-
-
-
 <style lang="scss" scoped>
-.filter{
-    display: flex;
-    justify-content: center;
+.filter {
+	display: flex;
+	justify-content: center;
 
-    &__item{
-        font-size: 2.2em;
-        text-transform: uppercase;
-        margin: 0 0.6em;
-        padding: 0.2em 0.6em;
+	&__item {
+		font-size: 2.2rem;
+		text-transform: uppercase;
+		margin: 0 0.6rem;
+		padding: 0.2rem 0.6rem;
 
-        &:hover{
-            background: gray;
-            cursor: pointer;
-        }
+		&:hover {
+			background: gray;
+			cursor: pointer;
+		}
 
-        &-isActive{
-            background: gray;
-        }
-
-    }
-
-
+		&-isActive {
+			background: gray;
+		}
+	}
 }
+.project {
+	display: flex;
+	justify-content: center;
+	border: solid 1px black;
+     position: relative;
 
+	&:hover::before {
+		background: white;
+		transition: all 0.5s ease-in-out;
+        height: 100%;
+	}
+	&::before {
+		content: '';
+		filter: brightness(60%);
+        opacity: 0.7;
+        position: absolute;
+        height: 0;
+        top: 0;
+        left: 0;
+        width: 100%;
+	}
+
+	&__container {
+		display: grid;
+		grid-template-columns: repeat(3, 1fr);
+		grid-auto-rows: 100px;
+		gap: 1rem;
+	}
+}
 </style>
